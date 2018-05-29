685b76ef81b60f242c008f5baf4448071b265abd
idris bellahsen <idris.bellahsen@gmail.com>
2018-05-29 12:09:39 +0200

    test

--- view/administrateurView.php
+++ view/administrateurView.php
@@ -4,11 +4,24 @@ ob_start(); ?>
 
    <div class="addUser">
     <b>Ajouter un utilisateur</b>
-    <form action="index.php?Administrateur" method="post" >
-        <input type="text" name="user_name" placeholder="Pseudo"><br>
-        <input type="text" name="password" placeholder="MotDePasse"><br>
-         <input type="text" name="email" placeholder="Email"><br>
-        <input type="submit" value="Ajouter utilisateur">
+        <form action="index.php" method="post" >
+        <input name="nom" type="text" placeholder="Nom"><br>
+        <input name="prenom" type="text" placeholder="Prenom"><br>
+        <input name="pseudo" type="text" placeholder="Pseudo"><br>
+        <input name="email" type="email" placeholder="Email"><br>
+        <input name="motdepass1" type="password" placeholder="Mot de passe"><br>
+        <input name="motdepass2" type="password" placeholder="Retape motdepass"><br>
+        <button type="submit">Inscription</button> <br>
+        <?php
+        if (isset($error)) {
+            if (is_array($error)) {
+                echo $error[2];
+            } else {
+                echo $error;
+            }
+        }
+        ?>
+
     </form>
     <br>
 
@@ -16,7 +29,7 @@ ob_start(); ?>
 include_once('../DAOSingleton/Requete.php');
 
     if (isset($_POST['user_name'])&&isset($_POST['password'])&&isset($_POST['email'])) {
-            if(Requete::inser("user","pseudo,password,email_inscription",$_POST['user_name'].$_POST['password'].$_POST['email_inscription'])){
+            if(Requete::inser("user","pseudo,password,email_inscription,id_user_type,id_information",$_POST['user_name'].$_POST['password'].$_POST['email_inscription'])){
                 echo "Utilisateur inscrit";
 
             }else{
